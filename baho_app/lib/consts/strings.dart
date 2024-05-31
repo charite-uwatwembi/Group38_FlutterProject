f8998d83043f5ce4bf901b748bb1c68b34e67a81
Kevin-gram <k.nyiringan@alustudent.com>
2024-05-31 19:41:34 +0200

    fix:fixing login alignments

--- baho_app/lib/consts/strings.dart
+++ baho_app/lib/consts/strings.dart
@@ -5,7 +5,7 @@ class AppStrings {
       signUp = "Sign Up",
       nameHint = "Don't mind giving us your FullName...",
       welcomeBack="Glad to have you back 😊!",
-      welcomMsg = "Welcome to baho app 😊!",
+      welcomMsg = " Hello welcome to Baho, how can we help you today😊!",
       haveAccount = "Have an account already?",
       getStarted = "Get Started by signingUp Now And become part of the Family 😊!",
       dontHaveAccount = "Don't have an account ?",
