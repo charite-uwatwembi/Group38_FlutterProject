d362bae8a37184b447f948861de59df679d21da6
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-06-19 20:48:54 +0200

    fixed some erros

--- baho_app/lib/views/categories_view/categories_view.dart
+++ baho_app/lib/views/categories_view/categories_view.dart
@@ -103,7 +103,7 @@ class CategoryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.blue[200],
+      color: Colors.blue[300],
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
