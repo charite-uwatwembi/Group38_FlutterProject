57774a79fab73be1470f890c2038508450248dde
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-06-24 18:06:10 +0200

    updated the feedback page

--- baho_app/lib/views/feedBack_view/feedback_view.dart
+++ baho_app/lib/views/feedBack_view/feedback_view.dart
@@ -18,109 +18,119 @@ class FeedbackPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: double.infinity,
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: CircleAvatar(
-                  radius: 80,
-                  backgroundImage: AssetImage('assets/images/logo.png'), // Replace with your logo path
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 100, // Adjust height to position avatar
+                  color: Colors.white,
                 ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 163, 200, 218), // Skyblue background color
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+                Positioned(
+                  top: 50, // Adjust to move avatar up or down
+                  child: CircleAvatar(
+                    radius: 80,
+                    backgroundImage: AssetImage('assets/images/logo.png'), // Replace with your logo path
+                  ),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 1),
-                    Text(
-                      'Baho',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                Container(
+                  margin: EdgeInsets.only(top: 130), // Adjust to fit avatar
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 163, 200, 218), // Skyblue background color
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
                     ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Rate your experience :)',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        IconButton(
-                          icon: Icon(Icons.sentiment_very_satisfied, color: Colors.orange, size: 30),
-                          onPressed: () {},
+                        SizedBox(height: 50), // Adjust to fit avatar
+                        Text(
+                          'Baho',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                        IconButton(
-                          icon: Icon(Icons.sentiment_satisfied, color: Colors.orange, size: 30),
-                          onPressed: () {},
+                        SizedBox(height: 20),
+                        Text(
+                          'Rate your experience :)',
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                         ),
-                        IconButton(
-                          icon: Icon(Icons.sentiment_neutral, color: Colors.orange, size: 30),
-                          onPressed: () {},
+                        SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.sentiment_very_satisfied, color: Colors.orange, size: 30),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.sentiment_satisfied, color: Colors.orange, size: 30),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.sentiment_neutral, color: Colors.orange, size: 30),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.sentiment_dissatisfied, color: Colors.orange, size: 30),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.sentiment_very_dissatisfied, color: Colors.orange, size: 30),
+                              onPressed: () {},
+                            ),
+                          ],
                         ),
-                        IconButton(
-                          icon: Icon(Icons.sentiment_dissatisfied, color: Colors.orange, size: 30),
-                          onPressed: () {},
+                        SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Comments:',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
                         ),
-                        IconButton(
-                          icon: Icon(Icons.sentiment_very_dissatisfied, color: Colors.orange, size: 30),
-                          onPressed: () {},
+                        TextField(
+                          maxLines: 4,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Handle Submit
+                            },
+                            child: Text('Submit', style: TextStyle(color: Colors.white)),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              minimumSize: Size(150, 40),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 80),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => HomeView()), // Replace with your HomeView
+                            );
+                          },
+                          child: Text('Home', style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            minimumSize: Size(250, 40),
+                          ),
                         ),
                       ],
                     ),
-                    SizedBox(height: 20),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Comments:',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    TextField(
-                      maxLines: 4,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle Submit
-                      },
-                      child: Text('Submit', style: TextStyle(color: Colors.white)),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        minimumSize: Size(150, 40),
-                      ),
-                    ),
-                    SizedBox(height: 80),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomeView()), // Replace with your HomeView
-                        );
-                      },
-                      child: Text('Home', style: TextStyle(color: Colors.white)),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        minimumSize: Size(250, 40),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
