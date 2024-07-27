037121b5659228f014b1f219327c554cdd1cc868
tuyishime johnson <j.tuyishime4@alustudent.com>
2024-07-27 15:50:09 +0200

    Firebase options

--- baho_app/lib/firebase_options.dart
+++ baho_app/lib/firebase_options.dart
@@ -7,9 +7,7 @@ import 'package:flutter/foundation.dart'
 
 ///
 
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
+
 /// await Firebase.initializeApp(
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
