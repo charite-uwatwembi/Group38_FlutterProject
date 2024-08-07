344fb4b74aaf5319167d8c8a55db9f2dff6735e1
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-08-07 17:31:28 +0200

    updated README

--- baho_app/lib/views/feedBack_view/feedback_view.dart
+++ baho_app/lib/views/feedBack_view/feedback_view.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:baho_app/views/settings_view/settings_view.dart';
 import 'package:baho_app/views/home_view/home_view.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
 class FeedbackPage extends StatelessWidget {
+  final TextEditingController _commentController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +16,7 @@ class FeedbackPage extends StatelessWidget {
         title: Text('FeedBack', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blue,
         iconTheme: IconThemeData(
-          color: Colors.white, // Set the back button color
+          color: Colors.white,
         ),
       ),
       body: SingleChildScrollView(
@@ -23,22 +28,20 @@ class FeedbackPage extends StatelessWidget {
               children: [
                 Container(
                   width: double.infinity,
-                  height: 100, // Adjust height  position of the  avatar
+                  height: 100,
                 ),
                 Positioned(
-                  top: 30, // Adjust to move avatar up or down
+                  top: 30,
                   child: CircleAvatar(
                     radius: 80,
-                    backgroundImage: AssetImage(
-                        'assets/images/logo.png'), // Replace with your logo path
+                    backgroundImage: AssetImage('assets/images/logo.png'),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 100), // Adjust to fit avatar
+                  margin: EdgeInsets.only(top: 100),
                   width: double.infinity,
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(
-                        108, 168, 211, 236), // Skyblue background color
+                    color: Color.fromARGB(108, 168, 211, 236),
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30),
                       topRight: Radius.circular(30),
@@ -49,45 +52,38 @@ class FeedbackPage extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        SizedBox(height: 20), // Adjust to fit avatar
+                        SizedBox(height: 20),
                         Text(
                           'Baho',
-                          style: TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.w900),
+                          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w900),
                         ),
                         SizedBox(height: 20),
                         Text(
                           'Rate your experience :)',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                         ),
                         SizedBox(height: 10),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             IconButton(
-                              icon: Icon(Icons.sentiment_very_satisfied,
-                                  color: Colors.orange, size: 30),
+                              icon: Icon(Icons.sentiment_very_satisfied, color: Colors.orange, size: 30),
                               onPressed: () {},
                             ),
                             IconButton(
-                              icon: Icon(Icons.sentiment_satisfied,
-                                  color: Colors.orange, size: 30),
+                              icon: Icon(Icons.sentiment_satisfied, color: Colors.orange, size: 30),
                               onPressed: () {},
                             ),
                             IconButton(
-                              icon: Icon(Icons.sentiment_neutral,
-                                  color: Colors.orange, size: 30),
+                              icon: Icon(Icons.sentiment_neutral, color: Colors.orange, size: 30),
                               onPressed: () {},
                             ),
                             IconButton(
-                              icon: Icon(Icons.sentiment_dissatisfied,
-                                  color: Colors.orange, size: 30),
+                              icon: Icon(Icons.sentiment_dissatisfied, color: Colors.orange, size: 30),
                               onPressed: () {},
                             ),
                             IconButton(
-                              icon: Icon(Icons.sentiment_very_dissatisfied,
-                                  color: Colors.orange, size: 30),
+                              icon: Icon(Icons.sentiment_very_dissatisfied, color: Colors.orange, size: 30),
                               onPressed: () {},
                             ),
                           ],
@@ -97,11 +93,11 @@ class FeedbackPage extends StatelessWidget {
                           alignment: Alignment.centerLeft,
                           child: Text(
                             'Comments:',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                         ),
                         TextField(
+                          controller: _commentController,
                           maxLines: 4,
                           decoration: InputDecoration(
                             border: OutlineInputBorder(),
@@ -112,10 +108,9 @@ class FeedbackPage extends StatelessWidget {
                           alignment: Alignment.centerRight,
                           child: ElevatedButton(
                             onPressed: () {
-                              // Handle Submit
+                              _submitFeedback(context);
                             },
-                            child: Text('Submit',
-                                style: TextStyle(color: Colors.white)),
+                            child: Text('Submit', style: TextStyle(color: Colors.white)),
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Colors.blue,
                               minimumSize: Size(150, 40),
@@ -127,13 +122,10 @@ class FeedbackPage extends StatelessWidget {
                           onPressed: () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      HomeView()), // Replace with your HomeView
+                              MaterialPageRoute(builder: (context) => HomeView()),
                             );
                           },
-                          child: Text('Home',
-                              style: TextStyle(color: Colors.white)),
+                          child: Text('Home', style: TextStyle(color: Colors.white)),
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blue,
                             minimumSize: Size(250, 40),
@@ -149,5 +141,31 @@ class FeedbackPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _submitFeedback(BuildContext context) async {
+    try {
+      
+      CollectionReference feedbackCollection = FirebaseFirestore.instance.collection('feedback');
+
+      
+      await feedbackCollection.add({
+        'comment': _commentController.text,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      
+      _commentController.clear();
+
+      
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Feedback submitted successfully')),
+      );
+    } catch (e) {
+      
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to submit feedback: $e')),
+      );
+    }
   }
 }
